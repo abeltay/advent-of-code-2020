77aaa278d9ffefc3fed42620cb631a1b21b7175c
Abel Tay <abel.tay@gmail.com>
2021-12-29 09:53:44 +0800

    Switch to use parsefile

--- 01/part1/runner.go
+++ 01/part1/runner.go
@@ -1,21 +1,16 @@
 package aoc
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"os"
 	"sort"
-	"strconv"
 )
 
 // Runner runs the algorithm to get the answer
-func Runner(data []string) int {
-	arr := make([]int, len(data))
-	for i := range data {
-		num, err := strconv.Atoi(data[i])
-		if err != nil {
-			log.Fatal(err)
-		}
-		arr[i] = num
-	}
+func Runner(filename string) int {
+	arr := parseFile(filename)
 	sort.Ints(arr)
 	for l := range arr {
 		h := len(arr) - 1
@@ -31,4 +26,26 @@ func Runner(data []string) int {
 		}
 	}
 	return 0
+}
+
+func parseFile(filename string) []int {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Could not open %q: %q", filename, err)
+	}
+	defer f.Close()
+
+	var arr []int
+	for {
+		var num int
+		_, err := fmt.Fscanln(f, &num)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		arr = append(arr, num)
+	}
+	return arr
 }
