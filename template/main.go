a787a7ddf324e77a91eec000b881861f1e198985
Abel Tay <15604207+abeltay@users.noreply.github.com>
2023-01-02 16:20:16 +0800

    Update template

--- template/main.go
+++ template/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-// parseFile reads the file and converts it to a format to use
-func parseFile(filename string) []int {
+func parseFile(filename string) []string {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("Could not open %q: %q\n", filename, err)
 	}
 	defer file.Close()
+
+	var arr []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		t := scanner.Text()
+		arr = append(arr, t)
+	}
+	return arr
+}
+
+func parseInput(filename string) []int {
+	input := parseFile(filename)
 
 	type line struct {
 		first  int
@@ -23,26 +34,19 @@ func parseFile(filename string) []int {
 	}
 
 	var arr []int
-	for {
+	for _, v := range input {
 		var l line
-		_, err := fmt.Fscanf(file, "%d-%d %c: %s", &l.first, &l.second, &l.letter, &l.text)
-		if err == io.EOF {
-			break
-		}
+		_, err := fmt.Sscanf(v, "%d-%d %c: %s", &l.first, &l.second, &l.letter, &l.text)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		arr = append(arr, l.first)
 	}
-	// scanner := bufio.NewScanner(file)
-	// for scanner.Scan() {
-	// 	t := scanner.Text()
-	// }
 	return arr
 }
 
 func part1(filename string) int {
-	input := parseFile(filename)
+	input := parseInput(filename)
 	fmt.Println(input)
 	var answer int
 	return answer
